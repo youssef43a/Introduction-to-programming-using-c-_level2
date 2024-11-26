42fef279a24972015277ea08afff2902a4adce9a
youssef43a <youssefennajar2@gmail.com>
2024-11-26 09:44:12 +0100

    add some file

---
+++ Source file/Vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../Header file/PrintOutput.h"
+
+using namespace std;
+
+int main() {
+    Vector::ft_VectorInistialization();
+    Vector::AddElementToVectors();
+    printf("\n//////////////////////////\n\n");
+    Vector::ft_PrintVectorElements();
+    return 0;
+}
